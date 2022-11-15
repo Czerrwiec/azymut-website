a077cdc1b17c5568d560ce86776e3e11b39fa804
Czerwiec <czerrwiec@gmail.com>
2022-11-15 12:25:54 +0100

    first commit

---
+++ js/script.js
@@ -0,0 +1,32 @@
+document.addEventListener("DOMContentLoaded", function(){
+
+    const nav = document.querySelector(".navbar")
+    const divCollapse = document.querySelector("#navbarNavAltMarkup")
+    const divLink = document.querySelectorAll(".nav-link")
+
+
+    function addShadow() {
+        if (window.scrollY >= 250) {
+        nav.classList.add("shadow-bg")
+        } else {
+            nav.classList.remove("shadow-bg")
+        }
+    }
+
+    
+    divLink.forEach(link => link.addEventListener("click", () => divCollapse.classList.remove("show")))
+
+
+
+
+
+
+    
+    window.addEventListener("scroll", addShadow)
+})
+
+ 
+
+
+
+    
